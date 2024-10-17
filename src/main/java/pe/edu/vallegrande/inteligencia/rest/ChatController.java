208d0126b6bd56e5abf86d6f0afd0bc5658795e0
Jose Gallardo <127469345+JoseGallardoPadron@users.noreply.github.com>
2024-10-17 08:36:03 -0500

    Update ChatController.java

--- src/main/java/pe/edu/vallegrande/inteligencia/rest/ChatController.java
+++ src/main/java/pe/edu/vallegrande/inteligencia/rest/ChatController.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chatbot")
+@RequestMapping("/chatbot")
 public class ChatController {
 
     private final ChatbotResponseService chatbotResponseService;
