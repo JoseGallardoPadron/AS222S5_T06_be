105767c8e2f2c58969b69eb1a8d7a802d5fc316c
Jose Gallardo <127469345+JoseGallardoPadron@users.noreply.github.com>
2024-10-16 22:50:20 -0500

    Update InteligenciaApplicationTests.java

--- src/test/java/pe/edu/vallegrande/inteligencia/InteligenciaApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/inteligencia/InteligenciaApplicationTests.java
@@ -2,12 +2,16 @@ package pe.edu.vallegrande.inteligencia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @SpringBootTest
 class InteligenciaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	    log.info("default test");
 	}
 
 }
